640cff13e126b5ae75f4f38bd69a8431fbefa39c
ja <ja@DESKTOP-NTO8PBS>
2022-08-19 07:10:24 +0200

    Dodaj pliki projektów.

---
+++ PetrolStation.Core/ViewModels/Controlls/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PetrolStation
+{
+    public class ProductViewModel 
+    {
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        //14:42
+        //   public string ProductName { get; set; }
+    }
+}
---
+++ PetrolStation.Core/ViewModels/Pages/ProductsPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PetrolStation.Core
+{
+    public class ProductsPageViewModel //: IReckoningInMIniliters
+    {
+       // public double Miniliter { get; set; }
+        List<ProductViewModel> ProductList { get; set; }
+    }
+}
